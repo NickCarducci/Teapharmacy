ec5bb24580cbe737622c2784134d73116c9c4715
Nick Carducci <nmcarducci@gmail.com>
2021-12-27 02:03:29 -0500

    Update Dropwire.js

--- src/Dropwire.js
+++ src/Dropwire.js
@@ -1,16 +1,22 @@
 import React from "react";
+import ExecutionEnvironment from "exenv";
 class Cable extends React.Component {
   constructor(props) {
     super(props);
     this.state = { limit: [], cache: null };
     this.page = React.createRef();
   }
+  componentDidMount() {
+    if (ExecutionEnvironment.canUseDOM) {
+      this.setState({ go: true });
+    }
+  }
   componentDidUpdate = (prevProps) => {
-    if (this.props.scrolling !== prevProps.scrolling) {
+    if (this.state.go && this.props.scrolling !== prevProps.scrolling) {
       const { cache } = this.state;
       const { scrollTopAndHeight, scrollTop, girth, timeout } = this.props;
-      var girt = girth ? girth : 8000;
-      var timeou = timeout ? timeout : 4000;
+      var girt = girth ? girth : 1000;
+      var timeou = timeout ? timeout : 1500;
       var continuee = this.props.fwd.current;
       if (!continuee && !cache) return;
       clearTimeout(this.setset);
@@ -22,17 +28,12 @@ class Cable extends React.Component {
         if (!continuee) continuee = cache;
         this.setState({ cache: continuee, between }, () => {
           if (!between && continuee) {
-            //while (page.firstChild) {
-            //page.removeChild(page.firstChild);
-            let onClick =
-              "ontouchstart" in continuee ? "touchstart" : "onclick";
-            if (continuee[onClick]) continuee.remove(); //touchevent
-            //continuee.click();
-            //}
-            return;
+            return continuee.remove(continuee);
           }
-          //if (!between && continuee) return continuee.remove();
-          if (page.children[0] !== this.state.cache)
+          if (
+            page.children &&
+            !page.children.find((s) => s === this.state.cache)
+          )
             page.appendChild(this.state.cache);
         });
       }, timeou);
@@ -44,17 +45,18 @@ class Cable extends React.Component {
   render() {
     const { between, continuee } = this.state;
     const { src, float, title, img } = this.props;
-    //const limited = limit.find((x) => x === Object.keys(this.props.fwd));
+    const onError = (e) => {
+      //this.props.fwd.current.remove();
+      this.props.onError(e);
+    };
     return (
       <div ref={this.page}>
         {!continuee || between ? (
           src === "" ? (
-            <span ref={this.props.fwd} style={{ border: "1px gray solid" }}>
-              {title}
-            </span>
+            <span style={{ border: "1px gray solid" }}>{title}</span>
           ) : img ? (
             <img
-              onError={this.props.onError}
+              onError={onError}
               alt={title}
               style={{
                 shapeOutside: "rect()",
@@ -69,7 +71,7 @@ class Cable extends React.Component {
             />
           ) : (
             <iframe
-              onError={this.props.onError}
+              onError={onError}
               title={title}
               style={{
                 shapeOutside: "rect()",
